811807c667a67df07e86ecbfb02f32052c63b619
Jaimie Lovett <jaimielovett@live.co.uk>
2024-07-10 14:20:51 +0100

    Fix typo in nvim-cpm.lua

--- .config/nvim/lua/jaimie/plugins/nvim-cmp.lua
+++ .config/nvim/lua/jaimie/plugins/nvim-cmp.lua
@@ -43,7 +43,7 @@ return {
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
-        { name = "codeium " },
+        { name = "codeium" },
       }),
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
