1ccb3cb43991190d308b47404036eeac8f71a0de
Jaimie Lovett <jaimielovett@live.co.uk>
2024-09-28 18:24:57 +0100

    Add zen-mode and twilight plugins

---
+++ .config/nvim/lua/jaimie/plugins/zen-mode.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		plugins = {
+			options = {
+				enabled = true,
+				ruler = false,
+				showcmd = false,
+				laststatus = 0,
+				number = false,
+				relativenumber = false,
+				signcolumn = "no",
+				cursorline = false,
+				cursorcolumn = false,
+				foldcolumn = "0",
+			},
+			tmux = { enabled = false },
+		},
+	},
+
+	vim.api.nvim_set_keymap("n", "<leader>z", ":ZenMode<CR>", { noremap = true, silent = true }),
+}
